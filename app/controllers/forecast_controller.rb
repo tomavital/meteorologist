97b9e7662cbc5fe9d933c9ba906226a547ace7d4
rails -v <c:sites>
2017-04-14 13:36:10 -0500

    hopo

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -21,13 +21,13 @@ class ForecastController < ApplicationController
 
     @current_temperature = parsed_data["currently"]["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
